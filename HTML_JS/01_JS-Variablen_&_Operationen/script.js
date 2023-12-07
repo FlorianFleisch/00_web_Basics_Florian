bd72cf08cbc2941ee37a3d930958a439714c407b
Florian <florian.fleisch@student.htldornbirn.at>
2023-12-07 11:23:41 +0100

    01_JS_Variablen_&_Operationen_finished

---
+++ HTML_JS/01_JS-Variablen_&_Operationen/script.js
@@ -0,0 +1,44 @@
+//Aufgaben:
+
+let a = 7;
+let b = 8;
+let c = 10;
+let d = a + b + c;
+let e = a + b;
+let e1 = e / c;
+let g = c - a;
+let g1 = g + b;
+let h = d * 3;
+let i = d / 4
+
+//Variablen
+
+// Berechne folgendes und gib das Eregebnis über console.log aus
+
+// a + b + c
+console.log("a + b + c = " + d);
+// (a + b) / c
+console.log("e / c = " + e1);
+// c -a + b
+console.log("g + b = " + g1);
+// a + b + c * 3
+console.log("d * 3 = " + h);
+// (a + b + c)/4
+console.log("d / 4 = " + i);
+
+// Ändere die Variablen auf folgende Werte
+
+// a = 3
+// b = a + 3
+// c = c -3
+a = 7;
+b = a + 3;
+c = c - 3;
+
+console.log(c);
+console.log(b);
+console.log(a);
+
+
+// gib die neuen Werte der Variablen aus
+
