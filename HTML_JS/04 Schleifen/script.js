76f288ef62560eb1cc63788cce260ed59a043c62
Florian <florian.fleisch@student.htldornbirn.at>
2023-12-14 11:11:39 +0100

    04 Schleifen_finished

---
+++ HTML_JS/04 Schleifen/script.js
@@ -0,0 +1,24 @@
+let a = 7;
+let b = 4.3;
+let name = " Florian";
+
+console.log(name + " " + b + a);
+
+for ( let count = 0;count<=10;count++){
+    console.log(count);
+}
+
+console.log("Florian");
+for ( let count = 10;count<=0;count++){
+    console.log(count);
+}
+
+
+// Zähle von 7 bis 45
+// Zähle von 9 bis 99
+// Zähle rückwärts von 10 bis 0
+// Zähle rückwärts von 1234 bis 1207
+
+// Gib Deinen Namen genau 8 mal aus
+// Zähle von 0 auf 10, dann gib Deinen Namen aus, dann zähle von 10 auf 0
+
