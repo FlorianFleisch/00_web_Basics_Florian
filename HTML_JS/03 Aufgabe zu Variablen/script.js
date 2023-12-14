b7a3fe6eebff7af1ae761f6b951701fad0cc6964
Florian <florian.fleisch@student.htldornbirn.at>
2023-12-14 10:48:49 +0100

    03 Aufgabe zu Variablen_finished

---
+++ HTML_JS/03 Aufgabe zu Variablen/script.js
@@ -0,0 +1,35 @@
+// Erstelle eine Variable banana mit dem Wert "Banane"
+// Erstelle eine Variable apple mit dem Wert "Apple"
+
+// Erstelle eine Variable bananaPricePerKilo mit dem Wert 2.14
+// Erstelle eine Variable applePricePerKilo mit dem Wert 3.43
+
+// Ein Apfel hat durchschnittlich ein Gewicht von 0.34 Kilo
+// Eine Banane hat durchschnittlich ein Gewicht von 0.22 Kilo
+
+// Gib folgendes aus: Anzahl Bananen/Äpfel pro Kilo, Preis pro Banane/Apfel
+// Preis von 8 Äpfeln
+// Preis von 17 Bananen
+// Preis von 1 Tonne Äpfel
+// Preis von 1 Tonne Bananen
+
+let banana = "Banane"
+let apple = "Apple"
+
+
+let bananaPricePerKilo = 2.14
+let  applePricePerKilo = 3.43
+
+let Äpfel8 = applePricePerKilo* 8
+
+console.log("8 Äpfel: " + Äpfel8);
+
+let Banane17 = bananaPricePerKilo* 17
+console.log("17 Bananen: " + Banane17);
+
+
+let TonneÄpfel = 2941* applePricePerKilo;
+console.log(TonneÄpfel);
+
+let TonneBananen = 2941* bananaPricePerKilo;
+console.log(TonneBananen);
