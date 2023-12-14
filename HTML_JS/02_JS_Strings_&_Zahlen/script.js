4857703d1650f817621818e2427a08720685fab1
Florian <florian.fleisch@student.htldornbirn.at>
2023-12-14 10:28:52 +0100

    02_JS_Strings&Zahlen_finished

---
+++ HTML_JS/02_JS_Strings_&_Zahlen/script.js
@@ -0,0 +1,36 @@
+let a = 7;
+let b = 8;
+
+let ab = a - b;
+
+
+let c = 7.4;
+let d = 7.5;
+
+result = c + d;
+console.log(result);
+
+let name = "Andreas"
+console.log(name);
+
+console.log("Das ergebnis von " + a + " und " + b + " ist " + ab);
+
+
+
+//Aufgabe 1
+
+let f = 17.4
+let g = 3.9
+resault1 = f / g;
+
+console.log(resault1);
+
+console.log("The rersult of " + f + " divided by " + g + " is " + resault1);
+
+//Aufgabe 2
+
+let firstname = "Florian"
+let secondname = " Fleisch"
+
+console.log("Ich heiße " + firstname
++ secondname);
