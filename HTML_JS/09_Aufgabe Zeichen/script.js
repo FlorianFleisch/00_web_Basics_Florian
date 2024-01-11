1a775aa18bf4554cc87fcb22fc95371bc0ce42c4
Florian <florian.fleisch@student.htldornbirn.at>
2024-01-11 11:25:22 +0100

    END OF LESSON

---
+++ HTML_JS/09_Aufgabe Zeichen/script.js
@@ -0,0 +1,33 @@
+
+//Erstelle mit Hilfe einer Schleife und Modulo das folgende Ausgabe liefert (Rechteckbeispiel adaptieren): 
+
+
+
+for (let index = 0; index < 1; index++) {
+    console.log("xxxx");
+    if(index%6==0);
+}
+
+for (let index = 0; index < 1; index++) {
+    console.log("OOOO");
+    if(index%6==0);
+}
+
+for (let index = 0; index < 1; index++) {
+    console.log("xxxx");
+    if(index%6==0);
+}
+
+for (let index = 0; index < 1; index++) {
+    console.log("OOOO");
+    if(index%6==0);
+}
+
+
+// 2. Versuche in einer for - Schleife (1-100) alle geraden Zahlen zu addieren (zwei Lösungswege ⇒ Zähler dahingehend verändern das er nur gerade Zahlen zählt und mit Modulo arbeiten.) 
+
+for (let index = 1; index < 100; index++) {
+    console.log();
+    
+    
+}
