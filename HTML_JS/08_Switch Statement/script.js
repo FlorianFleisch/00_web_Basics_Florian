0b3cc4ca047bcc68569c61ada3ba3645f99480e1
Florian <florian.fleisch@student.htldornbirn.at>
2024-01-11 10:52:25 +0100

    08_Switch Statement _finished

---
+++ HTML_JS/08_Switch Statement/script.js
@@ -0,0 +1,59 @@
+ // Erstelle eine Zufallszahl zwischen 5 und 10
+
+ let a = 6
+ 
+// Wenn der Wert 10 ist gib aus Ten
+
+if(a == 10){
+    console.log("Ten");
+}
+// Wenn der Wert 9 ist gib aus Nine
+
+if(a == 9){
+
+    console.log("Nine");
+}
+
+// Wenn der Wert 8 ist gib aus Eight
+
+if (a == 8) {
+    console.log("Eight");
+}
+// etc.
+if (a == 7) {
+    console.log("Seven");
+}
+
+if (a == 6) {
+    console.log("Six");
+}
+
+if (a == 5) {
+    console.log("Fife");
+
+}
+
+if (a == 4) {
+    console.log("Four");
+
+}
+
+if (a == 3) {
+    console.log("Three");
+
+}
+
+if (a == 2) {
+    console.log("Two");
+
+}
+
+if (a == 1) {
+    console.log("One");
+
+}
+
+if (a == 0) {
+    console.log("Zero");
+
+}
