34d49571af51c18d8e0295ea2e0dfe75b907dc09
Florian <florian.fleisch@student.htldornbirn.at>
2024-01-11 10:39:15 +0100

    07 Logische Operatoren_finished

---
+++ HTML_JS/07_Logische Operatoren/script.js
@@ -0,0 +1,25 @@
+// Erstelle zwei Zufallszahl zwischen 0 und 100
+
+// Speichere diese jeweils in einer Variable
+
+let a = 65;
+let b = 14;
+
+// Wenn die erste Zahl kleiner ist als die zweite UND die erste Zahl kleiner ist als 50
+// dann gib aus "Zahl 1 ist kleiner als Zahl 2 und Mini"
+
+if(a < b ||  50){
+    console.log("Zahl 1 ist kleiner als Zahl 2 und Mini");
+}
+// Wenn die erste Zahl kleiner ist als 30 oder die zweite Zahl kleiner ist als 30
+// dann gib aus "Eine der beiden ist kleiner als 30"
+
+if (a < 30 || b < 30){
+    console.log("Eine der beiden ist kleiner als 30");
+}
+// Wenn die erste Zahl kleiner ist als 50 UND die zweite Zahl ungleich 50 ist
+// dann gib aus "Erste Zahl klein, zweite kein 50iger"
+
+if(a < 50 || b != 50){
+    console.log("Erste Zahl klein, zweite kein 50iger");
+}
