4a7579991c66db7e977f6826c3f90feb44bd9512
Florian <florian.fleisch@student.htldornbirn.at>
2024-01-18 11:27:17 +0100

    END OF LESSON

---
+++ HTML_JS/10_Funktionen/script.js
@@ -0,0 +1,36 @@
+//Baue folgende Funktionen:
+
+// add(a,b)
+// subtract(a,b)
+// mulitply(a,b)
+// supercalculation(a,b) -> Formel: (a+b)/2 * a
+// printEasterDate(year) -> Diese Funktion bekommt das gewünschte Jahr und schreibt das Datum von Ostern.
+
+
+// Anbei ist die Formel zur Berechnung on Ostern. 
+
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function mulitply(a, b) {
+    return a * b;
+}
+
+function supercalculation(a, b) {
+   return a+b /2 * a;
+}
+
+
+function printEasterDate(year) {
+    return year;
+}
+
+
+
