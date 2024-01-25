86f731c7063763f7c09aee8c107d2f03ff9736db
Florian <florian.fleisch@student.htldornbirn.at>
2024-01-25 11:27:43 +0100

    END OF LESSON

---
+++ HTML_JS/11_Arrays/script.js
@@ -0,0 +1,32 @@
+// 	Erstelle ein Array mit folgenden Werten:
+//	4,1,2,3
+//	Füge die Werte 17 und 199 dazu
+//	
+//		a. Gib alle Werte in einer Schleife aus!
+//		b. Zähle alle Werte zusammen und gib das Ergebnis aus
+//		c. Berechne den Mittelwert der Zahlen und gib das Ergebnis aus
+
+
+let arr = [4, 1, 2, 3,]
+console.log(arr);
+
+arr.push(17, 199);
+console.log(arr);
+
+console.log(arr[2]);
+
+let data = arr.join(0,1,2,3,4,5,6)
+console.log(data);
+
+
+
+// 	Erstelle ein Array mit folgenden Strings:
+//	"Susi", "Paula", "Hans"
+//
+//	Gib folgenden Satz aus:
+//	"Meine Freunde sind Susi, Paula und Hans
+//
+//	Füge "Sepp" dazu. Nun soll die ausgabe folgendermaßen lauten:
+//	"Meine Freunde sind Susi, Paula, Hans und Sepp"
+
+
