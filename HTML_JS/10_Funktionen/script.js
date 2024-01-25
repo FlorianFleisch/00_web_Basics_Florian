e9187535d58ebe8967511268b8d4ccce2a0e90d8
Florian <florian.fleisch@student.htldornbirn.at>
2024-01-25 11:05:11 +0100

    10 Funktionen_finished

--- HTML_JS/10_Funktionen/script.js
+++ HTML_JS/10_Funktionen/script.js
@@ -24,13 +24,33 @@ function mulitply(a, b) {
 }
 
 function supercalculation(a, b) {
-   return a+b /2 * a;
+    return a + b / 2 * a;
 }
 
 
-function printEasterDate(year) {
-    return year;
+
+
+
+function printEasterDate(J) {
+
+    let N = J - 1900;
+    let A = N % 19;
+    let B = Math.floor((7 * A + 1) / 19);
+    let M = (11 * A + 4 - B) % 29;
+    let Q = Math.floor(N / 4);
+    let W = (N + Q + 31 - M) % 7;
+    let P = 25 - M - W;
+
+    if (P > 0) {
+        console.log(P + ".April");
+    }
+
+    else {
+        console.log(P + 31 + ".März");
+    }
+
 }
 
+printEasterDate(2024);
 
 
