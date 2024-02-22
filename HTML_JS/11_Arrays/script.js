825cc530f44aaccd308dc6ccdea0bc148be505c0
Florian <florian.fleisch@student.htldornbirn.at>
2024-02-22 11:21:00 +0100

    11 Arrays_finished

--- HTML_JS/11_Arrays/script.js
+++ HTML_JS/11_Arrays/script.js
@@ -19,20 +19,21 @@ console.log(arr);
         ergebniss += parseInt(arr[i]);
         console.log(ergebniss);
     }
-    
-
-
-
 
 
 
 // 	Erstelle ein Array mit folgenden Strings:
 //	"Susi", "Paula", "Hans"
+
+let arrp = ["Susi", "Paula", "Hans"];
+
 //
 //	Gib folgenden Satz aus:
 //	"Meine Freunde sind Susi, Paula und Hans
-//
-//	Füge "Sepp" dazu. Nun soll die ausgabe folgendermaßen lauten:
-//	"Meine Freunde sind Susi, Paula, Hans und Sepp"
 
+console.log("Meine Freunde sind " + arrp[0] + ", " +s arrp[1] + ", " + arrp[2]);
+//	Füge "Sepp" dazu. Nun soll die ausgabe folgendermaßen lauten:
+arrp.push("Sepp");
+//	"Meine Freunde sind Susi, Paula, Hans und Sepp"
+console.log("Meine Freunde sind " + arrp[0] + ", " + arrp[1] + ", " + arrp[2] + ", " + arrp[3]);
 
