b481a8b4b617cdb2bb9b81ce1d1ed5bdf5c8df04
Florian <florian.fleisch@student.htldornbirn.at>
2024-04-04 11:27:56 +0200

    LESSON FINISHED

--- HTML_JS/14 Tik Tak Toe/script.js
+++ HTML_JS/14 Tik Tak Toe/script.js
@@ -1,16 +1,54 @@
+import { read } from "fs";
+import { createInterface } from "readline";
+
+const readline = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const readLineAsync = () => {
+    return new Promise((resolve) => {
+        readline.question("", (userRes) => {
+            resolve(userRes);
+
+        });
+    });
+};
+
+
 let feld = [
     [0, 0, 0],
     [0, 0, 0],
     [0, 0, 0]
 ];
 
-for (let row = 0; row < feld.length; row++) {
-    let actualrow = feld[row];
-    let output = "";
-    for (let col = 0; col < actualrow.length; col++) {
+let activePlayer = 0;
 
-        output += actualrow[col] + "";
-    }
-    console.log(output);
+while (true) {
+    let input = await readLineAsync();
+    let arr = input.split(",");
+    let row = arr[0]
+    let col = arr[1]
+
+    console.log(arr);
+
+    feld[row][col] = 1;
+
+    console.log(input);
+    printField();
 }
+
+function printField() {
+
+    for (let row = 0; row < feld.length; row++) {
+        let actualrow = feld[row];
+        let output = "";
+        for (let col = 0; col < actualrow.length; col++) {
+
+            output += actualrow[col] + "";
+        }
+        console.log(output);
+    }
+}
+
 
