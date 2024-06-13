85f01f0e858519b4c087144f311a784d844a8a21
FlorianFleisch <florian.fleisch@student.htldornbirn.at>
2024-06-13 11:25:19 +0200

    END_OF_LESSON

---
+++ HTML&JS/01_ZugriffJSHTML/script.js
@@ -0,0 +1,33 @@
+
+
+
+let numb1 = document.getElementById("number1").value;
+let numb2 = document.getElementById("number2").value;
+
+
+let result = 0;
+
+function plus() {
+    let result = parseFloat(numb1) + parseFloat(numb2);
+    console.log(result);
+}
+
+function minus() {
+    result == parseFloat(numb1) - parseFloat(numb2); t;
+    console.log(result);
+}
+
+function multiplikation() {
+    result == parseFloat(numb1) * parseFloat(numb2);
+}
+
+function division() {
+    result == parseFloat(numb1) / parseFloat(numb2);
+}
+
+
+
+
+
+
+buttonClicked();
