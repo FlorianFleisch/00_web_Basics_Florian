4fccbafb5c3a993265994f5f6502a5d86d29b203
FlorianFleisch <florian.fleisch@student.htldornbirn.at>
2024-06-20 11:28:28 +0200

    01_TR_FINISHED

--- HTML&JS/01_ZugriffJSHTML/script.js
+++ HTML&JS/01_ZugriffJSHTML/script.js
@@ -1,33 +1,93 @@
-
-
-
-let numb1 = document.getElementById("number1").value;
-let numb2 = document.getElementById("number2").value;
-
-
+let inputField = document.getElementById("numbers");
+let calculation = ' ';
 let result = 0;
 
+function calculate() {
+    const inputField = document.getElementById('numbers');
+    const calculation = inputField.value;
+    try {
+        const result = eval(calculation);
+        inputField.value = result;
+    } catch (e) {
+        inputField.value = "Error";
+    }
+}
+
+function one() {
+    calculation += '1';
+    inputField.value = calculation;
+}
+
+function two() {
+    calculation += '2';
+    inputField.value = calculation;
+}
+
+function three() {
+    calculation += '3';
+    inputField.value = calculation;
+}
+
+function four() {
+    calculation += '4';
+    inputField.value = calculation;
+}
+
+function five() {
+    calculation += '5';
+    inputField.value = calculation;
+}
+
+function six() {
+    calculation += '6';
+    inputField.value = calculation;
+}
+
+function seven() {
+    calculation += '7';
+    inputField.value = calculation;
+}
+
+function eight() {
+    calculation += '8';
+    inputField.value = calculation;
+}
+
+function nine() {
+    calculation += '9';
+    inputField.value = calculation;
+}
+
+function zero() {
+    calculation += '0';
+    inputField.value = calculation;
+}
+
+
+function reset() {
+    calculation = ' ';
+    inputField.value = calculation;
+}
+
 function plus() {
-    let result = parseFloat(numb1) + parseFloat(numb2);
-    console.log(result);
+    calculation += '+';
+    inputField.value = calculation;
 }
 
 function minus() {
-    result == parseFloat(numb1) - parseFloat(numb2); t;
-    console.log(result);
+    calculation += '+';
+    inputField.value = calculation;
 }
 
 function multiplikation() {
-    result == parseFloat(numb1) * parseFloat(numb2);
+    calculation += '*';
+    inputField.value = calculation;
 }
 
 function division() {
-    result == parseFloat(numb1) / parseFloat(numb2);
+    calculation += '/';
+    inputField.value = calculation;
 }
 
 
 
-
-
-
-buttonClicked();
